f2a87f137d3e5187bd7f5e9460f2fa42480e75af
guidoasbun <gdrodrigo8@gmail.com>
2020-03-15 20:54:00 -0700

    UserItem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
 import './App.css';
 
 class App extends React.Component{
   render() {
     return (
     <div className="App">
-    <h1>Hello World!!</h1>
+        <Navbar/>
+        <Users/>
     </div>
   );
   }
