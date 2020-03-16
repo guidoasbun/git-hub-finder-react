5f845ed6c4dd0c7688ab1824cf58dc2b89fdec04
guidoasbun <gdrodrigo8@gmail.com>
2020-03-15 21:37:22 -0700

    github request

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,38 +2,22 @@ import React, { Component } from 'react'
 import UserItem from './UserItem'
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      },
-      {
-        id: '3',
-        login: 'djhyett',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/djhyett'
-      }
-    ]
-  }
 
   render() {
     return (
-      <div>
-        {this.state.users.map(user => (
+      <div style={userStyle}>
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user}/>
         ))}
       </div>
     )
   }
+}
+
+const userStyle = { 
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
 }
 
 export default Users
